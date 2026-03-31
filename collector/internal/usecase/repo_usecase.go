47fb53119b9712a1eb4352deeaf7748825496024
iraedeus <dtotjmyanin@mail.ru>
2026-03-31 11:50:06 +0300

    feat(collector): format date to 'HH:MM:SS DD.MM.YY' format

--- collector/internal/usecase/repo_usecase.go
+++ collector/internal/usecase/repo_usecase.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"time"
+
 	"golang-course/collector/internal/domain"
 )
 
@@ -18,6 +20,20 @@ func NewCollectorUseCase(p RepoProvider) *CollectorUseCase {
 	}
 }
 
+func formatData(date string) string {
+	t, err := time.Parse(time.RFC3339, date)
+	if err != nil {
+		return date
+	}
+	return t.Format("15:04:05 02.01.06")
+}
+
 func (u *CollectorUseCase) Execute(owner, repoName string) (domain.Repo, error) {
-	return u.provider.GetRepoInfo(owner, repoName)
+	repo, err := u.provider.GetRepoInfo(owner, repoName)
+	if err != nil {
+		return repo, err
+	}
+
+	repo.CreatedAt = formatData(repo.CreatedAt)
+	return repo, err
 }
